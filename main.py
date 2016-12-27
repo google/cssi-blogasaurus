b22f4402510795d42ecf875cdf1e1f8712ef7db8
Taymon A. Beal <taymon@google.com>
2016-12-27 13:22:16 -0500

    Revert from step 6 (Users API) to step 5 (templates).
    In this step, users cannot sign in.
    
    Change-Id: I1126e1a2ad323984cc3e545191981aaab0c279fb

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import logging
 
 import flask
-from google.appengine.api import users
 
 import posts
 
@@ -11,33 +10,14 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    user = users.get_current_user()
-    if user:
-        nickname = user.nickname()
-        auth_url = users.create_logout_url(dest_url=flask.url_for('index'))
-    else:
-        nickname = None
-        auth_url = users.create_login_url(dest_url=flask.url_for('index'))
-    return flask.render_template(
-        'index.html',
-        nickname=nickname,
-        is_admin=users.is_current_user_admin(),
-        auth_url=auth_url,
-        posts=posts.POSTS)
+    return flask.render_template('index.html', posts=posts.POSTS)
 
 
 @app.route('/post/<int:post_id>')
 def view_post(post_id):
     if 0 <= post_id - 1 < len(posts.POSTS):
-        if users.get_current_user():
-            sign_in_url = None
-        else:
-            sign_in_url = users.create_login_url(
-                dest_url=flask.url_for('view_post', post_id=post_id))
         return flask.render_template(
-            'view_post.html',
-            post=posts.POSTS[post_id - 1],
-            sign_in_url=sign_in_url)
+            'view_post.html', post=posts.POSTS[post_id - 1])
     else:
         return flask.render_template('404.html'), 404
 
