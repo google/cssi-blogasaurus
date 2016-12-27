4c35b369dc9e67859bb94a09a80e1ebd1bfd51a8
Taymon A. Beal <taymon@google.com>
2016-12-27 13:13:44 -0500

    Revert from step 7 (Datastore) to step 6 (Users API).
    In this step, posts are hardcoded and there are no comments.
    
    Change-Id: I9830786ea79e31e0e67aa80b51e66a8f88b0adcb

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import logging
 
 import flask
 from google.appengine.api import users
-from google.appengine.ext import ndb
 
-import models
+import posts
 
 
 app = flask.Flask(__name__)
@@ -24,68 +23,23 @@ def index():
         nickname=nickname,
         is_admin=users.is_current_user_admin(),
         auth_url=auth_url,
-        posts=models.Post.query().order(-models.Post.posted_at).fetch())
+        posts=posts.POSTS)
 
 
-@app.route('/post/<post_id>')
+@app.route('/post/<int:post_id>')
 def view_post(post_id):
-    post_key = ndb.Key(urlsafe=post_id)
-    post = post_key.get()
-    if post:
-        comments = (models.Comment.query(models.Comment.post == post_key)
-                    .order(models.Comment.posted_at).fetch())
+    if 0 <= post_id - 1 < len(posts.POSTS):
         if users.get_current_user():
             sign_in_url = None
         else:
             sign_in_url = users.create_login_url(
                 dest_url=flask.url_for('view_post', post_id=post_id))
         return flask.render_template(
-            'view_post.html', post=post, comments=comments,
+            'view_post.html',
+            post=posts.POSTS[post_id - 1],
             sign_in_url=sign_in_url)
     else:
         return flask.render_template('404.html'), 404
-
-
-@app.route('/new-post')
-def new_post():
-    if not users.is_current_user_admin():
-        return flask.render_template('403.html'), 403
-    return flask.render_template('new_post.html')
-
-
-@app.route('/submit-post', methods=['POST'])
-def submit_post():
-    if not users.is_current_user_admin():
-        return flask.render_template('403.html'), 403
-    post = models.Post(
-        title=flask.request.form['title'],
-        content=flask.request.form['content'])
-    post_key = post.put()
-    return flask.redirect(
-        flask.url_for('view_post', post_id=post_key.urlsafe()))
-
-
-@app.route('/submit-comment', methods=['POST'])
-def submit_comment():
-    user = users.get_current_user()
-    if not user:
-        return flask.render_template('403.html'), 403
-    post_key = ndb.Key(urlsafe=flask.request.form['post_id'])
-    if not post_key.get():
-        return flask.render_template('400.html'), 400
-    author = models.Author(nickname=user.nickname(), email=user.email())
-    comment = models.Comment(
-        post=post_key, author=author, content=flask.request.form['content'])
-    comment.put()
-    best_match = flask.request.accept_mimetypes.best_match(
-        ['application/json', 'text/html'])
-    if (best_match == 'application/json' and
-        flask.request.accept_mimetypes[best_match] >
-        flask.request.accept_mimetypes['text/html']):
-        return flask.jsonify(nickname=author.nickname, email=author.email)
-    else:
-        return flask.redirect(
-            flask.url_for('view_post', post_id=post_key.urlsafe()))
 
 
 @app.errorhandler(400)
