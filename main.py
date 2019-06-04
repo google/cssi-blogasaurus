676c83898678677340af48f56588cca33b9973b7
Riccardo Crepaldi <crepric@google.com>
2019-06-04 10:21:45 -0700

    Adding templates, step 5

--- main.py
+++ main.py
@@ -17,7 +17,27 @@ class AboutMyFamilyHandler(webapp2.RequestHandler):
         template = the_jinja_env.get_template('templates/about_my_family.html')
         self.response.write(template.render())
 
+class BlogHandler(webapp2.RequestHandler):
+    def get(self):
+        template = the_jinja_env.get_template('templates/new_post.html')
+        self.response.write(template.render())
+    def post(self):
+        title_input = self.request.get('title')
+        content_input = self.request.get('content')
+        name_input = self.request.get('name')
+
+        template_vars = {
+            'title': title_input,
+            'name': name_input,
+            'content': content_input,
+        }
+        template = the_jinja_env.get_template(
+            'templates/view_post.html')
+        self.response.write(template.render(template_vars))
+
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/family', AboutMyFamilyHandler),
+    ('/new', BlogHandler),
 ], debug=True)
